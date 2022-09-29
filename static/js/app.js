f06cf44f5d09784596c9495edbf399d613fadce7
Ben Johnson <bjohnson8904@gmail.com>
2022-09-28 20:09:56 -0500

    added metadata

--- static/js/app.js
+++ static/js/app.js
@@ -82,19 +82,31 @@ function drawBubbleChart(sampleId)
 //--- SHOW METADATA FUNCTION START-----------
 function showMetaData(sampleId)
 {
+    // consoloe.log to test
+    console.log(`showMetaData(${sampleId})`);
+    let metaData = d3.select("#sample-metadata")
+    // let selector = d3.select("#sample-metadata")
     d3.json(url).then(data => {
         let samples = data.metadata;
         let resultArray = samples.filter(s => s.id == sampleId);
         let result = resultArray[0];
-        let id = result.id;
+        let id = result.id;        
         let ethnicity = result.ethnicity;
         let gender = result.gender;
-        let age = result.age;
+        let age = result.age;        
         let location = result.location;
         let bbtype = result.bbtype;
         let wfreq = result.wfreq;
+        metaData.text(`id: ${id}`)
+        metaData.append("li").text(`ethnicity: ${ethnicity}`)
+        metaData.append("li").text(`gender: ${gender}`)
+        metaData.append("li").text(`age: ${age}`)
+        metaData.append("li").text(`location: ${location}`)
+        metaData.append("li").text(`bbtype: ${bbtype}`)
+        metaData.append("li").text(`wfreq: ${wfreq}`)     
+        d3.selectAll("li").style("list-style-type", "none")
     })
-    console.log(`showMetaData(${sampleId})`);
+    
 
 }
 //--- SHOW METADATA FUNCTION END-----------
@@ -110,7 +122,6 @@ function drawGauge(sampleId)
 function optionChanged(sampleId)
 {
     console.log(`optionChanged, new value: ${sampleId}`);
-
     drawBarGraph(sampleId);
     drawBubbleChart(sampleId);
     showMetaData(sampleId);
@@ -128,7 +139,6 @@ function InitDashboard()
    
     d3.json(url).then(function(data) {
         console.log("Here's the data", data);
-
         let sampleNames = data.names;
         console.log(sampleNames);
         // populate dropdown
@@ -222,110 +232,3 @@ InitDashboard()
 //         };
 //     };
 // });
-
-// // horizontal bar chart
-// // ------------------------------------
-// let trace1 = {
-//     x: reversedData.map(object => object.greekSearchResults),
-//     y: reversedData.map(object => object.greekName),
-//     text: reversedData.map(object => object.greekName),
-//     name: "Greek",
-//     type: "bar",
-//     orientation: "h"
-//   };
-// // ------------------------------------
-
-
-// // drop down
-// // ------------------------------------
-// // Initializes the page with a default plot
-// function init() {
-//     data = [{
-//       x: [1, 2, 3, 4, 5],
-//       y: [1, 2, 4, 8, 16] }];
-  
-//     Plotly.newPlot("plot", data);
-//   }
-  
-//   // Call updatePlotly() when a change takes place to the DOM
-//   d3.selectAll("#selDataset").on("change", updatePlotly);
-  
-//   // This function is called when a dropdown menu item is selected
-//   function updatePlotly() {
-//     // Use D3 to select the dropdown menu
-//     let dropdownMenu = d3.select("#selDataset");
-//     // Assign the value of the dropdown menu option to a variable
-//     let dataset = dropdownMenu.property("value");
-  
-//     // Initialize x and y arrays
-//     let x = [];
-//     let y = [];
-  
-//     if (dataset === 'dataset1') {
-//       x = [1, 2, 3, 4, 5];
-//       y = [1, 2, 4, 8, 16];
-//     }
-  
-//     else if (dataset === 'dataset2') {
-//       x = [10, 20, 30, 40, 50];
-//       y = [1, 10, 100, 1000, 10000];
-//     }
-  
-//     // Note the extra brackets around 'x' and 'y'
-//     Plotly.restyle("plot", "x", [x]);
-//     Plotly.restyle("plot", "y", [y]);
-//   }
-  
-//   init();
-// // ------------------------------------
-
-
-// // looping
-// // ------------------------------------
-// for (let i = 0; i < searchResults.length; i++) {
-//     row = searchResults[i];
-//     names.push(row.pair);
-//     greekNames.push(row.greekName);
-//     romanNames.push(row.romanName);
-//     greekSearchResults.push(row.greekSearchResults);
-//     romanSearchResults.push(row.romanSearchResults);
-//   }
-// // ------------------------------------
-
-
-// // Functions and built in functions
-// // ------------------------------------
-// function doubleAddition(c, d) {
-//     let total = addition(c, d) * 2;
-  
-//     return total;
-//   }
-  
-//   // Log results of doubleAddition function
-//   console.log(doubleAddition(3, 4));
-  
-  
-//   // Javascript built in functions
-//   let longDecimal = 112.34534454;
-//   let roundedDecimal = Math.floor(longDecimal);
-//   console.log(roundedDecimal);
-// // ------------------------------------
-
-
-
-// // plotting
-// // ------------------------------------
-// let trace1 = {
-//     x: books,
-//     y: timesRead,
-//     type: 'bar'
-//   };
-  
-//   let data = [trace1];
-  
-//   let layout = {
-//     title: title
-//   };
-  
-//   Plotly.newPlot("plot", data, layout);
-//   // ------------------------------------
